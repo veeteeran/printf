37d986f25aed92a44aa8cae2982d7e641cc6015d
GabrielAbdul <gabrielabdul1@gmail.com>
2020-03-16 15:40:58 +0000

    corrected seg fault

---
+++ gabriels_dir/_printf.c
@@ -0,0 +1,43 @@
+#include "holberton.h"
+/**
+ *
+ *
+ *
+ */
+int _printf(const char *format, ...)
+{
+	fmt_t types[] = {
+		{"c", print_char},
+		{"s", print_string},
+		{NULL, NULL}
+	};
+	int i, j, count = 0;
+	va_list vl;
+
+	va_start(vl, format);
+	i = 0;
+	while (format && format[i])
+	{
+		if (format[i] != '%')
+		{
+			_putchar(format[i]);
+		}
+		else
+		{
+			j = 0;
+			while (types[j].s)
+			{
+				if (format[i + 1] == *(types[j].s))
+				{
+					count += types[j].fptr(vl);
+					i++; /* move i to the placeholder */
+				}
+				j++;
+			}
+		}
+		i++;
+	}
+	va_end(vl);
+	_putchar('\n');
+	return (count);
+}
