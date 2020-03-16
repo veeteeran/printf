1b6f82b30ffdc7b76e07808e717f83fa8c2eb900
Viet Tran <vietnamtran@gmail.com>
2020-03-16 23:39:53 +0000

    Add _printf prototype

--- holberton.h
+++ holberton.h
@@ -15,6 +15,7 @@ typedef struct format
 	int (*fptr)(va_list);
 } fmt_t;
 
+int _printf(const char *format, ...);
 int _putchar(char c);
 int print_char(va_list args);
 int print_string(va_list args);
