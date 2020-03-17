0c6be2d6fc28a4168d19e94cd49c9d8eb873edd8
GabrielAbdul <gabrielabdul1@gmail.com>
2020-03-17 22:54:44 +0000

    Betty space at start of line fixes

--- _printf.c
+++ _printf.c
@@ -8,49 +8,60 @@
  */
 int _printf(const char *format, ...)
 {
-        int i, count = 0;
-        va_list vl;
-        if (format == NULL)
-                return (-1);
-        va_start(vl, format);
-        i = 0;
-        while (format[i])
-        {
-                if (format[i] != '%')
-                        count += _putchar(format[i]);
-                else
-                {
-                        i++; /* move i to next char */
-                        if (format[i] == '\0')
-                                return (-1);
-                        else if (format[i] == '%')
-                               _putchar('%');
+	int i, count = 0;
+	va_list vl;
+
+	if (format == NULL)
+		return (-1);
+	va_start(vl, format);
+	i = 0;
+	while (format[i])
+	{
+		if (format[i] != '%')
+			count += _putchar(format[i]);
+		else
+		{
+			i++; /* move i to next char */
+			if (format[i] == '\0')
+				return (-1);
+			else if (format[i] == '%')
+				_putchar('%');
 			else
 				count += get_format(format[i], vl);
 		}
-                i++;
-        }
-        va_end(vl);
-        return (count);
+		i++;
+	}
+	va_end(vl);
+	return (count);
 }
+
+/**
+ * get_format - depending on format, it returns the call to
+ * different functions to print different types
+ *
+ * @format: format specifier
+ * @vl: arguement to print
+ *
+ * Return: count of printed chars
+ */
 
 int get_format(char format, va_list vl)
 {
-	int i = 0, count;
+	int i = 0, count
 
-	 fmt_t types[] = {
-                {'c', print_char},
-                {'s', print_string},
-                {'d', print_dec},
-                {'i', print_int},
-                {'\0', '\0'}
-        };
+	fmt_t types[] = {
+		{'c', print_char},
+		{'s', print_string},
+		{'d', print_dec},
+		{'i', print_int},
+		{'\0', '\0'}
+	};
 
 	while (types[i].s)
 	{
 		if (format == types[i].s)
 		{
-                	count = types[i].fptr(vl);
+			count = types[i].fptr(vl);
 			return (count);
 		}
 		i++;
